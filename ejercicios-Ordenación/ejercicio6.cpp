bfd214677260a8dc7385ac0461ad76ea171b34d1
Dncz <alu0101328348@ull.edu.es>
2023-06-18 15:01:07 +0100

    ejercicio 6

--- ejercicios-Ordenación/ejercicio6.cpp
+++ ejercicios-Ordenación/ejercicio6.cpp
@@ -25,7 +25,6 @@ void selection(std::vector<int> &v, int ini) {
     int min = i;
     for (unsigned int j = i + 1; j < v.size(); j++) {
       if (v[j] < v[min]) {
-        // std::cout << "Si " << v[j] << " < " << v[min] << ", entonces min = " << j << std::endl;
         min = j;
       }
     }
@@ -33,12 +32,53 @@ void selection(std::vector<int> &v, int ini) {
   }
 }
 
+void mezcla(std::vector<int> &v, unsigned cen) {
+  unsigned i = 0;
+  unsigned j = cen + 1;
+  unsigned k = 0;
+
+  std::vector<int> aux(v.size() + 1);
+
+  while (i <= cen && j <= v.size()) {
+    if (v[i] < v[j]) {
+      aux[k] = v[i];
+      i++;
+    } else {
+      aux[k] = v[j];
+      j++;
+    }
+    k++;
+  }
+
+  if (i > cen) {
+    while (j <= v.size()) {
+      aux[k] = v[j];
+      j++;
+      k++;
+    }
+  } else {
+    while (i <= cen) {
+      aux[k] = v[i];
+      i++;
+      k++;
+    }
+  }
+
+  for (unsigned i = 0; i < v.size(); i++) {
+    v[i] = aux[i];
+  }
+
+}
+
+
 int main(void) {
+
   std::vector<int> v = {4, 7, 6, 5, 3, 8, 9, 10, 1, 2};
   int center = v.size() / 2;
   std::cout << "center: " << center << std::endl;
   // primera mitad con inserccion
   insertion(v, center - 1);
+  std::cout << "Vector después de insercción: " << std::endl;
   for (auto i : v) {
     std::cout << i << " ";
   }
@@ -46,12 +86,20 @@ int main(void) {
 
   // segunda mita con selccion
   selection(v, center);
+  std::cout << "Vector después de selección: " << std::endl; 
   for (auto i : v) {
     std::cout << i << " ";
   }
   std::cout << std::endl;
 
   // FALTA MEZCLAR LAS DOS MITADES
+  mezcla(v, (v.size() / 2) - 1);
+
+  std::cout << "Vector mezclado:" << std::endl;
+  for(auto i : v) {
+    std::cout << i << " ";
+  }
+  std::cout << std::endl;
 
   return 0;
 }
