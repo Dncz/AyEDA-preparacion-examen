70a568b80e211c20353951440c6cc84fac2853b7
Dncz <alu0101328348@ull.edu.es>
2023-06-18 18:14:05 +0100

    funcion mezcla

--- ejercicios-Ordenación/funciones.cpp
+++ ejercicios-Ordenación/funciones.cpp
@@ -124,17 +124,107 @@ void insercion4(std::vector<int> &v, unsigned cen) {
 
 // quicksort, merge, mezcla de merge, shellsort
 
+/////////////////////// FUNCIÓN DE QUICKSORT ///////////////////////
+void quicksort(std::vector<int> &v, int ini, int fin) {
+  int i = ini;
+  int f = fin;
+  int pivote = v[(i + f) / 2];
+
+  while (i <= f) {
+    // recorrido descendente
+    while (v[i] < pivote) i++;
+    // recorrido ascendente
+    while (v[f] > pivote) f--;
+
+    if (i <= f) {
+      std::swap(v[i], v[f]);
+      i++;
+      f--;
+    }
+  }
+
+  if (ini <= f) quicksort(v, ini, f);
+  if (i < fin) quicksort(v, i, fin);
+}
+
+/////////////////////////////////////////////////////////////////////
+
+/////////////////////// FUNCIÓN DE SHELLSORT ///////////////////////
+void deltaMethod(std::vector<int> &v, int delta) {
+  for (unsigned i = delta; i < v.size(); i++) {
+    int key = v[i];
+    unsigned j = i;
+    while ( j >= delta && key < v[j - delta] ) {
+      v[j] = v[j - delta];
+      j = j - delta;
+    }
+    v[j] = key;
+  }
+}
+
+void shellsort(std::vector<int> &v) {
+  int delta = v.size();
+  while (delta > 1) {
+    delta = delta / 2;
+    deltaMethod(v, delta);
+  }
+}
+/////////////////////////////////////////////////////////////////////
+
+/////////////////////// FUNCIÓN DE MEZCLA ///////////////////////
+// mezcla de menor a mayor
+void mezcla(std::vector<int> &v, unsigned cen) {
+  unsigned i = 0;
+  unsigned j = cen + 1;
+  unsigned k = 0;
+
+  std::vector<int> aux(v.size() + 1);
+
+  while (i <= cen &&j <= v.size()) {
+    if (v[i] < v[j]) {
+      aux[k] = v[i];
+      i++;
+    } else {
+      aux[k] = v[j];
+      j++;
+    }
+    k++;
+  }
+
+  if (i > cen) {
+    while (j <= v.size()) {
+      aux[k] = v[j];
+      j++;
+      k++;
+    }
+  } else {
+    while (i <= cen) {
+      aux[k] = v[i];
+      i++;
+      k++;
+    }
+  }
+  for (unsigned i = 0; i < v.size(); i++) {
+    v[i] = aux[i];
+  }
+
+}
+/////////////////////////////////////////////////////////////////////
+
 int main(void) {
 
   std::vector<int> v = {4, 7, 6, 5, 3, 8, 9, 10, 1, 2};
   std::cout << "Centro: " << v[v.size() / 2 -1] << std::endl;
 
-  // seleccionSort(v, v.size() / 2);
-  // seleccionSort2(v, v.size() / 2);
+  seleccionSort(v, v.size() / 2);
+  seleccionSort2(v, v.size() / 2);
   // seleccionSort3(v, v.size() / 2);
   // seleccionSort4(v, v.size() / 2);
 
-  insercion4(v, v.size() / 2);
+  // insercion4(v, v.size() / 2);
+  // quicksort(v, 0, v.size()- 1);
+  // shellsort(v);
+  mezcla(v, (v.size() / 2) - 1);
 
   for(auto i : v) {
     std::cout << i << " ";
